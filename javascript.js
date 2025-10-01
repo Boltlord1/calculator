2b9dce848b2985eecbd80798a7831bc7d671b558
Boltlord <80578277+Boltlord1@users.noreply.github.com>
2025-10-01 09:15:56 +0500

    made the calculator fully functional

--- javascript.js
+++ javascript.js
@@ -1,32 +1,3 @@
-function add(one, two) {
-    return one + two
-}
-
-function subtract(one, two) {
-    return one - two
-}
-
-function multiply(one, two) {
-    return one * two
-}
-
-function divide(one, two) {
-    if (two === 0) return
-    return one / two
-}
-
-function operate(one, two, operator) {
-    if (operator === `add`) {
-        return add(one, two)
-    } else if (operator === `subtract`) {
-        return subtract(one, two)
-    } else if (operator === `multiply`) {
-        return multiply(one, two)
-    } else if (operator === `divide`) {
-        return divide(one, two)
-    }
-}
-
 const previous = document.querySelector(`.previous`)
 const current = document.querySelector(`.current`)
 
@@ -52,8 +23,54 @@ const numbers = [
     document.querySelector(`.zero`)
 ]
 
+let firstNumber = undefined
+let secondNumber = undefined
+let selectedOperator = ``
+let output = 0
+
+function clear() {
+    firstNumber = undefined
+    secondNumber = undefined
+    selectedOperator = ``
+    output = 0
+    previous.textContent = ``
+    current.textContent = ``
+}
+
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].onclick = () => {
         current.textContent = `${current.textContent}${numbers[i].textContent}`
+        secondNumber = Number(current.textContent)
     }
 }
+
+function operate(operator) {
+    if (firstNumber !== undefined && selectedOperator === operator) {
+        switch (operator) {
+            case `plus`:
+                output = firstNumber + secondNumber
+            break
+            case `minus`:
+                output = firstNumber - secondNumber
+            break
+            case `star`:
+                output = firstNumber * secondNumber
+            break
+            case `slash`:
+                output = firstNumber / secondNumber
+        }
+    } else if (firstNumber === undefined) {
+        output = secondNumber
+    }
+    previous.textContent = output
+    current.textContent = ``
+    selectedOperator = operator
+    firstNumber = output
+}
+
+functions.plus.onclick = () => operate(`plus`)
+functions.minus.onclick = () => operate(`minus`)
+functions.star.onclick = () => operate(`star`)
+functions.slash.onclick = () => operate(`slash`)
+functions.equals.onclick = () => operate(selectedOperator)
+functions.clear.onclick = () => clear()
